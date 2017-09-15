90dd5d18f9c683fe27d4296d713797dc1faf0a6f
Kui Yang <c6swift@gmail.com>
2017-09-15 16:30:16 +0800

    添加动态修改字体大小的功能

--- src/touch.js
+++ src/touch.js
@@ -1,8 +1,12 @@
 document.querySelector('.buttons').addEventListener('click', touch());
 const result = document.querySelector('.result');
+const screenWidth = document.querySelector('.screen').clientWidth;
+result.style.fontSize = `${Math.floor(screenWidth / 6 * 1.3)}px`;
+
+const inputElement = document.querySelector('input');
 function touch() {
   let prevTouch = null;
-  return function (e) {
+  return function(e) {
     const { target: button } = e;
     if (button.classList.contains('operator')) {
       button.classList.add('active');
@@ -15,8 +19,12 @@ function touch() {
       numberPass(button);
     }
     prevTouch = button;
-  }
+  };
 }
+
+document
+  .querySelector('.ac')
+  .addEventListener('click', () => { result.textContent = 0; changeFont();});
 
 function numberPass(target) {
   let number = target.textContent;
@@ -25,12 +33,13 @@ function numberPass(target) {
   let prevShow = result.textContent.split(',');
   if (prevShow.length === 1 && prevShow[0][0] === '0') {
     prevShow[0] = [];
-  } 
+  }
   result.textContent = addComma(prevShow, number);
+  // inputElement.value = result.textContent;
+  changeFont();
 }
 
 function addComma(result, input) {
-  console.log(result.clientElement);
   result = result.reverse().join('');
   result += input;
 
@@ -40,4 +49,15 @@ function addComma(result, input) {
     show.push(result.slice(i * 3, (i + 1) * 3));
   }
   return show.reverse().join(',');
+}
+
+function changeFont() {
+  let resultWidth = result.offsetWidth;
+  if (resultWidth > screenWidth) {
+    const trueLength = result.textContent.length - 2;
+    result.style.fontSize = `${Math.floor(screenWidth / trueLength * 1.3)}px`;
+    console.log(result.style.fontSize);
+  } else {
+    result.style.fontSize = `${Math.floor(screenWidth / 6 * 1.3)}px`;
+  }
 }
